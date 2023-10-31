d55f50e63a391c7bba565c05b65dbca4148bf890
ahmedgamhan <144797061+ahmedgamhan@users.noreply.github.com>
2023-10-31 11:37:34 +0300

    000

--- src/chart.jsx
+++ src/chart.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import ReactApexChart from 'react-apexcharts';
 
- function ApexChart() {
-    const series = [44, 55, 13, 43, 22];
-    const options = {
-      chart: {
-        width: 380,
-        type: 'pie',
-      },
-      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200,
-            },
-            legend: {
-              position: 'bottom',
-            },
+function ApexChart() {
+  const series = [60000, 40000, 20000];
+  const options = {
+    chart: {
+      width: 480,
+      type: 'pie',
+    },
+    labels: ['الإيرادات ', ' المصروفات', 'صافي الربـــح'],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: 'bottom',
           },
         },
-      ],
-    };
-  
-    return (
-      <div id="chart">
-        <ReactApexChart options={options} series={series} type="pie" width={380} />
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title">حركة المصروفات والإيرادات</h4>
+          <div id="chart">
+            <ReactApexChart options={options} series={series} type="pie" width={480} />
+          </div>
+        </div>
       </div>
-    );
-  }
-  export default ApexChart;
+    </div>
+  );
+}
+
+export default ApexChart;
